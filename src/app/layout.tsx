6d5d3a8bcf33642f87a449c1f8612a5e28ae3d79
adarraji <a.darraji@outlook.com>
2024-03-08 22:51:29 -0500

    created navbar, ntofication and footer components and added them to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import Notification from "@/components/notification/Notification"
+import Navbar from "@/components/navbar/Navbar"
+import Footer from "@/components/footer/Footer"
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Notification />
+        <Navbar />
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
