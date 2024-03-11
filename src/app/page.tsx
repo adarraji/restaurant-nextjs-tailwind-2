ef271c21aafb25a0931870f102e6bdd171a1f074
adarraji <a.darraji@outlook.com>
2024-03-10 22:05:00 -0400

    removed unused image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Featured from "@/components/featured/Featured"
 import Offer from "@/components/offer/Offer"
 import Slider from "@/components/slider/Slider"
-import Image from 'next/image'
 
 export default function Home() {
   return (
