53b46367949fa97933b2fff01274aa0c08881ea0
adarraji <a.darraji@outlook.com>
2024-03-11 23:31:34 -0400

    adding slider and styles

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,8 +1,56 @@
-import styles from "./slider.module.css"
+"use client"
+import Image from "next/image"
+import s from "./slider.module.css"
+import { useEffect, useState } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide(prev => prev === data.length - 1 ? 0 : prev + 1)
+    }, 3000)
+
+    return () => clearInterval(interval)
+
+  }, [])
+
+
+
   return (
-    <div>Slider</div>
+    <div className={s.slider_container}>
+
+      {/* TEXT CONTAINER */}
+      <div className={s.text_container}>
+        <h1>{data[currentSlide].title}</h1>
+        <button >Order Now</button>
+      </div>
+
+      {/* IMAGE CONTAINER */}
+      <div className={s.image_container}>
+        <Image src={data[currentSlide].image} alt="" fill />
+      </div>
+
+
+    </div>
   )
 }
 
