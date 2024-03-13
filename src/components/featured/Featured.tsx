a1139bf232b7615fbd4995a84f764c4b5f04b32c
adarraji <a.darraji@outlook.com>
2024-03-12 22:55:55 -0400

    adding featred items

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,8 +1,40 @@
-import styles from "./featured.module.css"
+import Image from "next/image"
+import s from "./featured.module.css"
+import { featuredProducts } from "@/data"
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={s.container}>
+
+      {/* WRAPPER */}
+      <div className={s.wrapper}>
+
+        {featuredProducts.map(item => (
+
+          // SINGLE ITEM
+          <div key={item.id} className={s.single_item}>
+
+            {/* IMAGE CONTAINER */}
+            {item.img && (
+              <div className={s.image_container}>
+                <Image src={item.img} alt="" fill sizes="(max-width: 640px) 100vw, 100vw"/>
+              </div>
+            )}
+
+            {/* TEXT CONTAINER */}
+            <div className={s.text_container}>
+              <h1>{item.title}</h1>
+              <p>{item.desc}</p>
+              <span>${item.price}</span>
+              <button>Add to Cart</button>
+            </div>
+
+          </div>
+        ))}
+
+      </div>
+
+    </div>
   )
 }
 
