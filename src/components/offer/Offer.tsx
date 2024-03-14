1e4b82bec487d5e299c2208d8adc29c38c778af0
adarraji <a.darraji@outlook.com>
2024-03-13 23:21:07 -0400

    adding offer component

--- src/components/offer/Offer.tsx
+++ src/components/offer/Offer.tsx
@@ -1,8 +1,24 @@
-import styles from "./offer.module.css"
+import Image from "next/image"
+import s from "./offer.module.css"
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div className={s.container}>
+
+      {/* TEXT CONTAINER */}
+      <div className={s.text_container}>
+        <h1>Delicious Burger & French Fries</h1>
+        <p> Progressively simplify effective e-toilers and process-centric methods of empowerment. Quickly pontificate parallel.</p>
+        <span>Countdown</span>
+        <button>Order Now</button>
+      </div>
+
+      {/* IMAGE CONTAINER */}
+      <div className={s.image_container}>
+        <Image src="/offerProduct.png" alt="" fill sizes="100vw, 100vw" />
+      </div>
+
+    </div>
   )
 }
 
