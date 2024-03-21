cc74bb567a1ef5a84118231abc753fb3c9609ee3
adarraji <a.darraji@outlook.com>
2024-03-20 20:15:25 -0400

    Prodduct list page

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import Link from "next/link"
+import s from "./category.module.css"
+import { pizzas } from "@/data"
+import Image from "next/image"
 
 const CategoryPage = () => {
     return (
-        <div>CategoryPage</div>
+        <div className={s.container}>
+            {
+                pizzas.map(item => (
+                    <Link href={`/product/${item.id}`} className={`${s.link}  group odd:bg-fuchsia-50`}>
+                        {
+                            item.img
+                            && <div className={s.image_container}>
+                                <Image src={item.img} alt="" fill priority sizes="100w, 100vw" />
+                            </div>
+                        }
+
+                        <div className={s.text_container}>
+                            <h1>{item.title}</h1>
+                            <h2 className="group-hover:hidden">{item.price}</h2>
+                            <button className="hidden group-hover:block">Add to Cart</button>
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
     )
 }
 
