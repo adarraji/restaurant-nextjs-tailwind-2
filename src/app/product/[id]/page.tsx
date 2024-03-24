a797bd28e6a9eccaedce7cf1f226826f4bff4b87
adarraji <a.darraji@outlook.com>
2024-03-24 01:16:13 -0400

    signle product

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { singleProduct } from "@/data"
+import s from "./product.module.css"
+import Image from "next/image"
+
 
 const SingleProductPage = () => {
     return (
-        <div>SingleProductPage</div>
+        <div className={s.container}>
+            <div className={s.image_container}>
+                {
+                    singleProduct.img
+                    && <Image src={singleProduct.img} alt="" fill sizes="100vw, 100vw" />
+                }
+            </div>
+            <div className={s.text_container}>
+                <h1>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+                <span>{singleProduct.price}</span>
+            </div>
+        </div>
     )
 }
 
