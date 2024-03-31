2cd127300efba991a87696247e104c171ca85dde
adarraji <a.darraji@outlook.com>
2024-03-31 18:35:02 -0400

    fix

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,8 +1,6 @@
 
-import { PrismaClient } from "@prisma/client"
+import prisma from "@/app/menu/utils/connect"
 import { NextResponse } from "next/server"
-
-const prisma = new PrismaClient
 
 // FETCH ALL CATEGORIES
 export const GET = async () => {
