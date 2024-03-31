03e0048902e44d3f433262da5032503671792cd9
adarraji <a.darraji@outlook.com>
2024-03-30 21:00:09 -0400

    orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import s from "./orders.module.css"
 
 const OrdersPage = () => {
   return (
-    <div>OrdersPage</div>
+    <div className={s.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-red-50">
+            <td>1237861238721</td>
+            <td>19.07.2023</td>
+            <td>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
+          <tr className="odd:bg-gray-100">
+            <td>1237861238721</td>
+            <td>19.07.2023</td>
+            <td>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
+          <tr className="odd:bg-gray-100">
+            <td>1237861238721</td>
+            <td>19.07.2023</td>
+            <td>89.90</td>
+            <td>Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
+            <td>On the way (approx. 10min)...</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
